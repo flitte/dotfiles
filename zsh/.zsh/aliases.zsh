a0cd7d31cbb65182d8559d2a69d11f4325b73243
flitte <flitte@users.noreply.github.com>
2016-09-30 11:11:55 +0000

    New dotfiles

---
+++ zsh/.zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# ls
+alias ls="ls --colour=auto"
+alias ll="ls -l"
+
+# grep
+alias grep="grep --color"
+
+#zsh
+alias zshrc="$EDITOR ~/.zshrc"
+
+# git
+alias g="git"
+alias ga="git add"
+alias gc="git commit -v"
+alias gcm="git commit -v -m"
+alias gb="git branch"
